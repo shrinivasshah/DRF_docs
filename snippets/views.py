61c758720cfbb5ad192b8aa94e8dca6f4d2db89d
shrinivasshah <shrinivasshah05@gmail.com>
2021-02-26 13:22:59 +0530

    added urls.py

--- snippets/views.py
+++ snippets/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 # from rest_framework.renders import JSONRenderer
-from rest_framework.parser import JSONParser
-from snippets.models import Snippet
-from snippet.serializers import SnippetSerialier
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
 # Create your views here.
 
 
@@ -12,13 +12,36 @@ from snippet.serializers import SnippetSerialier
 def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
-        serializer = SnippetSerialier(snippets, many=True)
+        serializer = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = SnippetSerialier(data=data)
+        serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data)
+        if serializer.is_valid():
+            serializer.save()
+            JsonResponse(serializer.data)
+        return JsonResponse(serializer.data, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
